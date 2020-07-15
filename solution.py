073dbd4f54870e8d2af941ce7bca0676f379b2ee
Valeriia <v.zhovkovskaya@gmail.com>
2020-07-15 17:26:14 +0200

    modify solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def predict(data):
     """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)
     TODO: modify it
     """
-    result = data['col1'] - data['col2']
+    result = data['col1'] + data['col2']
     return result
 
 
